b37ec52aba87629f54c17aaef6743eac091d071d
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-07-12 11:23:12 +0530

    add assets & setup navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <img src={assets.logo} alt="/" className="logo" />
+      <ul className="navbar-menu">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile App</li>
+        <li>Contact Us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+          <div className="navbar-search-icon">
+            <img src={assets.basket_icon} alt="" />
+            <div className="dot"></div>
+          </div>
+          <button>Sign In</button>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
