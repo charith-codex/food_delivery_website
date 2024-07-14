a676a2cfbcd6af739cbc94c1e1246710354923ff
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-07-14 12:00:05 +0530

    add routes

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="header-contents">
+            <h2>Bringing your favorite dishes right to your doorstep!</h2>
+            <p>Explore a variety of cuisines from the comfort of your home. Indulge in mouth-watering flavors delivered right to your door!</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
