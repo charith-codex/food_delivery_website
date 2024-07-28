6a9004bdfcbb9f7011428fef7bfcb124b5b998e5
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-07-28 16:07:41 +0530

    add styles to footer & setup app download section

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
-import "./Home.css";
-import Header from "../../components/Header/Header";
-import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
-import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
+import React, { useState } from 'react';
+import './Home.css';
+import Header from '../../components/Header/Header';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+import AppDownload from '../../components/AppDownload/AppDownload';
 
 const Home = () => {
-  const [category, setCategory] = useState("All");
+  const [category, setCategory] = useState('All');
 
   return (
     <div>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   );
 };
